a9c734228829b1fbd8d1b903800d60c40b23c692
Dishari Mukherjee <impeccablemischief@gmail.com>
2026-04-12 20:46:51 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        oneStep, twoStep = 1,1
+        for step in range(n-1):
+            temp = oneStep
+            oneStep = oneStep + twoStep
+            twoStep = temp
+        return oneStep
+        
